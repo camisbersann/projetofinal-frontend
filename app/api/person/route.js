edc4da150389755d2314c9d1ac761294e174a9e7
Enrico070 <125027577+Enrico070@users.noreply.github.com>
2023-11-27 15:29:02 -0300

    feat: route.js and page.js

--- app/api/person/route.js
+++ app/api/person/route.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
-const url = process.env.BASE_URL + "/person";
+const url = process.env.BASE_URL + "/aboutus";
 
 export async function GET() {
     try{
