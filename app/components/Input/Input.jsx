12474aee634e68a2c3fe4dc7a4ca5bbc3966db7a
Andrey Botero <andrey.botero2018@gmail.com>
2023-12-11 08:43:02 -0300

    inputs

--- app/components/Input/Input.jsx
+++ app/components/Input/Input.jsx
@@ -4,17 +4,20 @@ import styles from './Input.module.css'
 const InputRegisters = ({ type, varName, setVarName, label }) => {
     return (
 
-       
+
+
+
 
         <div className={styles.group}>
+            <input className={styles.input}
+                type={type}
+                value={varName}
+                onChange={(e) => setVarName(e.target.value)} />
+            <span className={styles.highlight}></span>
+            <span className={styles.bar}></span>
             <label className={styles.label} htmlFor={varName}>
                 {label}:
             </label>
-            <input className={styles.input}
-             type={type} 
-             value={varName}
-              onChange= {(e) => setVarName(e.target.value)}
-             />
         </div>
 
 
