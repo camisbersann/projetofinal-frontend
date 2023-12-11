6ab15a9028ffc1c64e5a454110a27faa1eec5d9f
Andrey Botero <andrey.botero2018@gmail.com>
2023-12-11 16:43:24 -0300

    Add Footer component and update styles

--- app/person/register/page.jsx
+++ app/person/register/page.jsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import { Header } from "@/app/components/header/Header";
 import InputRegisters from "@/app/components/Input/Input";
 import Buttons from "@/app/components/Button/Button";
+import Footer from "@/app/components/footer/Footer";
+
 
 export default function Register() {
     const [name, setName] = useState("");
@@ -51,34 +53,40 @@ export default function Register() {
         <>
             <Header />
 
-            <div className={styles.actions}>
-                <Link href="/person">
-                    <Buttons titulo={"Voltar para Membros"} />
-                </Link>
+            <div className={styles.body}>
+                <div className={styles.container}>
+
+                    <div className={styles.actions}>
+                        <Link href="/person">
+                            <Buttons titulo={"Voltar para Membros"} />
+                        </Link>
+                    </div>
+
+                    <div className={styles.personContainer}>
+                        <h1 className={styles.mainText}>Cadastrar Membro</h1>
+
+                        <form onSubmit={handleSubmit}>
+                            <InputRegisters type={"text"} varName={name} setVarName={setName} label={'Nome'} />
+
+                            <InputRegisters type={"number"} varName={age} setVarName={setAge} label={'Idade'} />
+
+                            <InputRegisters type={"text"} varName={email} setVarName={setEmail} label={'Email'} />
+
+                            <InputRegisters type={"text"} varName={instagram} setVarName={setInstagram} label={'Instagram'} />
+
+                            <InputRegisters type={"text"} varName={position} setVarName={setPosition} label={'Posição'} />
+
+                            <InputRegisters type={"text"} varName={description} setVarName={setDescription} label={'Descrição'} />
+
+                            <InputRegisters type={"text"} varName={image} setVarName={setImage} label={'Imagem'} />
+
+                            <Buttons type={"submit"} titulo={"Enviar formulário"} />
+
+                        </form>
+                    </div>
+                </div>
             </div>
-
-            <div className={styles.personContainer}>
-                <h1 className={styles.mainText}>Cadastrar Membro</h1>
-
-                <form onSubmit={handleSubmit}>
-                    <InputRegisters type={"text"} varName={name} setVarName={setName} label={'Nome'}/>
-
-                    <InputRegisters type={"number"} varName={age} setVarName={setAge} label={'Idade'}/>
-
-                    <InputRegisters type={"text"} varName={email} setVarName={setEmail} label={'Email'}/>
-
-                    <InputRegisters type={"text"} varName={instagram} setVarName={setInstagram} label={'Instagram'}/>
-
-                    <InputRegisters type={"text"} varName={position} setVarName={setPosition} label={'Posição'}/>
-
-                    <InputRegisters type={"text"} varName={description} setVarName={setDescription} label={'Descrição'}/>
-
-                    <InputRegisters type={"text"} varName={image} setVarName={setImage} label={'Imagem'}/>
-
-                    <Buttons type={"submit"}  titulo={"Enviar formulário"} />
-
-                </form>
-            </div>
+            <Footer />
         </>
 
     )
